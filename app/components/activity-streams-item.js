740a3649b0c3333cdb3c0b596f3bd2cf2b729dfa
Ricky Hurtado <odatruh.ykcir@gmail.com>
2016-09-09 16:38:38 +0800

    Show the post(s) on click on author or category in activity streams

--- app/components/activity-streams-item.js
+++ app/components/activity-streams-item.js
@@ -1,5 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'li'
+  tagName: 'li',
+  author: '',
+  authorId: 0,
+  activityAction: '',
+  blogType: '',
+  blogRoute: '',
+  blogId: 0,
+  createdAt: '',
+  didReceiveAttrs: function(){
+    let activity = this.activity;
+    let category = activity.category_name.toLowerCase();
+
+    this.set('author', activity.author);
+    this.set('authorId', activity.author_id);
+    this.set('blogType', category);
+    this.set('blogRoute', category + 's.show');
+    this.set('blogId', activity.blog_id);
+    this.set('activityAction', activity.action);
+    this.set('createdAt', activity.created_at);
+  }
 });
