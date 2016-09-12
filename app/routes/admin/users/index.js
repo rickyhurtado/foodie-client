3250a68bd5e20c7627e7b76dd8db66ca4c4c1773
Ricky Hurtado <odatruh.ykcir@gmail.com>
2016-09-13 00:06:08 +0800

    Create model and pagination for admin users list

--- app/routes/admin/users/index.js
+++ app/routes/admin/users/index.js
@@ -6,6 +6,23 @@ export default Ember.Route.extend(AuthSessionMixin, AuthenticatedRouteMixin, {
   activate: function() {
     document.title = 'Users | Admin | Ora HQ';
   },
+  offset: 0,
+  limit: 5,
+  queryParams: {
+    page: {
+      refreshModel: true
+    }
+  },
+  model: function(params){
+    let page = 1;
+
+    if (params.page){
+      page = params.page;
+      page = isNaN(page) ? 1 : Math.floor(Math.abs(page));
+    }
+
+    return this.get('store').query('user', { limit: this.get('limit'), offset: page });
+  },
   renderTemplate: function(controller, model){
     this._super(controller, model);
     this.initAuthSession();
