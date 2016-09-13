eefbc0571fed4af293138d75bcf486662172837d
Ricky Hurtado <odatruh.ykcir@gmail.com>
2016-09-14 00:16:00 +0800

    Add UI logic to new user form

--- app/components/admin-user-form.js
+++ app/components/admin-user-form.js
@@ -1,28 +1,101 @@
 import Ember from 'ember';
+import AlertMessageMixin from '../mixins/alert-message-mixin';
 const { service } = Ember.inject;
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(AlertMessageMixin, {
   tagName: '',
-  router: service('-routing'),
+  activeAdmin: '',
+  activeEditor: 'active',
+  inputClassFirstName: 'form-control',
+  inputClassLastName: 'form-control',
+  inputClassEmail: 'form-control',
+  inputClassPassword: 'form-control',
   buttonDisabled: false,
+  router: service('-routing'),
+  didInsertElement: function(){
+    Ember.run.later(function(){
+      window.scrollTo(0, 0);
+    }, 100);
+  },
   backToUsers: function(){
     this.get('router').transitionTo('admin.users');
   },
-  saveUserForm: function(){
+  validateUserForm: function(){
+    let errors = [];
+    let message = '';
+
+    if (this.get('firstName') === undefined){
+      errors.push('First name is required.');
+      this.set('inputClassFirstName', 'form-control error');
+    }
+
+    if (this.get('lastName') === undefined){
+      errors.push('Last name is required.');
+      this.set('inputClassLastName', 'form-control error');
+    }
+
+    if (this.get('email') === undefined){
+      errors.push('Email is required.');
+      this.set('inputClassEmail', 'form-control error');
+    }
+
+    if (this.get('password') === undefined){
+      errors.push('Password is required.');
+      this.set('inputClassPassword', 'form-control error');
+    }
+
+    if (errors.length > 0){
+      message = '<ul><li>' + errors.join('</li><li>') + '</li>';
+      this.setAlertMessage('danger', message);
+      return false;
+    }
+
+    return true;
+  },
+  createUser: function(){
     return true;
   },
   actions: {
-    saveUser: function(){
-      let result = this.saveUserForm();
+    submitNewUser: function(){
+      let self = this;
+      let result = self.validateUserForm();
 
-      this.set('buttonDisabled', true);
+      if (result === false){
+        return false;
+      }
+
+      self.setAlertMessage('warning', 'Saving user...');
+      self.set('buttonDisabled', true);
+
+      result = self.createUser();
 
       if (result){
-        this.backToUsers();
+        self.setAlertMessage('success', 'New user is successfully created.');
+
+        Ember.run.later(function(){
+          self.backToUsers();
+        }, 3000);
+      }
+    },
+    clickRole: function(role){
+      this.set('activeEditor', '');
+      this.set('activeAdmin', '');
+      this.set('active' + role, 'active');
+      this.set('role', role);
+    },
+    cancelUser: function(){
+      let result = confirm('You have unsaved data. Do you want to proceed?');
+
+      if (result){
+        self.backToUsers();
       }
     },
     deleteUser: function(){
-      this.backToUsers();
+      let result = confirm('Are you sure you want to delete this user?');
+
+      if (result){
+        history.back();
+      }
     }
   }
 });
