27227724c8105bc745820b04bbdc2a1649404957
Ricky Hurtado <odatruh.ykcir@gmail.com>
2016-09-16 00:23:58 +0800

    Modify the blogs controller

--- app/controllers/blogs.js
+++ app/controllers/blogs.js
@@ -1,15 +1,20 @@
 import Ember from 'ember';
+import AuthSessionMixin from '../mixins/auth-session-mixin';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(AuthSessionMixin, {
   getAjax: function(options){
-    let page = options && options.page ? '?page%5Bnumber%5D=' + options.page : '';
+    let self = this;
+    let timestamp = Date.now();
+    let page = options && options.page ? `?page%5Bnumber%5D=${options.page}` : '';
 
     return new Ember.RSVP.Promise(function(resolve, reject){
       Ember.$.ajax({
         type: 'GET',
-        url: '/' + options.route + page,
-        cache: true,
+        url: `/${options.route}${page}`,
+        cache: false,
         headers: {
+          'AUTHORIZATION': self.get('currentUser.token'),
+          'EMAIL': self.get('currentUser.email'),
           'Content-Type': 'application/vnd.api+json',
           'Accept': 'application/vnd.api+json'
         },
