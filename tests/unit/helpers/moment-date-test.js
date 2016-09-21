39dd068641a251069c3228dcebfe14f8ca414bcf
Ricky Hurtado <odatruh.ykcir@gmail.com>
2016-09-21 21:58:46 +0800

    Make all helper tests to be passing using real tests
    Helpers:
    formatted-date
    moment-date
    truncated-text (updated)
    unescaped-html

--- tests/unit/helpers/moment-date-test.js
+++ tests/unit/helpers/moment-date-test.js
@@ -3,8 +3,28 @@ import { module, test } from 'qunit';
 
 module('Unit | Helper | moment date');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = momentDate([42]);
-  assert.ok(result);
+test('formats date with moment JS using "second" as "startOf" option "fromNow"', function(assert) {
+  assert.expect(9);
+
+  let dateNow = new Date();
+  let timeNow = dateNow.getSeconds();
+  let secondsAgo = momentDate([dateNow.setSeconds(timeNow - 7)], { format: 'second'});
+  let minuteAgo = momentDate([dateNow.setSeconds(timeNow - 60)], { format: 'second'});
+  let minutesAgo = momentDate([dateNow.setSeconds(timeNow - 90)], { format: 'second'});
+  let hourAgo = momentDate([dateNow.setSeconds(timeNow - 3600)], { format: 'second'});
+  let hoursAgo = momentDate([dateNow.setSeconds(timeNow - 7200)], { format: 'second'});
+  let dayAgo = momentDate([dateNow.setSeconds(timeNow - (3600 * 24))], { format: 'second'});
+  let daysAgo = momentDate([dateNow.setSeconds(timeNow - (3600 * 24 * 3))], { format: 'second'});
+  let monthAgo = momentDate([dateNow.setSeconds(timeNow - (3600 * 24 * 31))], { format: 'second'});
+  let monthsAgo = momentDate([dateNow.setSeconds(timeNow - (3600 * 24 * 200))], { format: 'second'});
+
+  assert.equal(secondsAgo, 'a few seconds ago');
+  assert.equal(minuteAgo, 'a minute ago');
+  assert.equal(minutesAgo, '3 minutes ago');
+  assert.equal(hourAgo, 'an hour ago');
+  assert.equal(hoursAgo, '3 hours ago');
+  assert.equal(dayAgo, 'a day ago');
+  assert.equal(daysAgo, '4 days ago');
+  assert.equal(monthAgo, 'a month ago');
+  assert.equal(monthsAgo, '8 months ago');
 });
--- tests/unit/helpers/truncate-text-test.js
+++ tests/unit/helpers/truncate-text-test.js
@@ -3,8 +3,11 @@ import { module, test } from 'qunit';
 
 module('Unit | Helper | truncate text');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = truncateText([42]);
-  assert.ok(result);
+test('truncates long text if "length" option is defined', function(assert) {
+  assert.expect(1);
+
+  let text = '&lt;p&gt;The quick brown fox jumps over the lazy dog.&lt;/p&gt;';
+  let newText = truncateText([text], { length: 20 });
+
+  assert.equal(newText, 'The quick brown fox...');
 });
