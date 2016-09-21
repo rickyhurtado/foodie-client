e0e9cef75443149bc994011ab40dd21709a979db
Ricky Hurtado <odatruh.ykcir@gmail.com>
2016-09-21 21:55:09 +0800

    Remove unused helpers

--- app/helpers/activity-url.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function activityUrl(params) {
-  return '/' + params[0].toLowerCase() + 's/' + params[1];
-}
-
-export default Ember.Helper.helper(activityUrl);
--- app/helpers/string-to-lower.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function stringToLower(string) {
-  return string[0].toLowerCase();
-}
-
-export default Ember.Helper.helper(stringToLower);
