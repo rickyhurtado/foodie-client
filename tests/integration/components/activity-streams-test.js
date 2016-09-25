1b087c3f09d7732c72be5a023f70018bfa39af90
Ricky Hurtado <odatruh.ykcir@gmail.com>
2016-09-25 14:00:44 +0800

    Add tests for activity-streams and activity-streams-item components

--- tests/integration/components/activity-streams-test.js
+++ tests/integration/components/activity-streams-test.js
@@ -5,20 +5,40 @@ moduleForComponent('activity-streams', 'Integration | Component | activity strea
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+const dateNow = new Date();
+const timeNow = dateNow.getSeconds();
+const firstCreatedAt = dateNow.setSeconds(timeNow - 30);
+const firstActivity = {
+  author_id: 1,
+  author: 'Foodie Blogger',
+  blog_id: 1,
+  action: 'published',
+  created_at: firstCreatedAt,
+  category_name: 'Recipe',
+  deleted: false
+};
+const secondCreatedAt = dateNow.setSeconds(timeNow - 7200);
+const secondActivity = {
+  author_id: 2,
+  author: 'Foodie Reviewer',
+  blog_id: 2,
+  action: 'deleted',
+  created_at: secondCreatedAt,
+  category_name: 'Review',
+  deleted: false
+};
+const model = [firstActivity, secondActivity];
 
-  this.render(hbs`{{activity-streams}}`);
+test('it renders list of activity streams items', function(assert) {
+  assert.expect(1);
 
-  assert.equal(this.$().text().trim(), '');
+  this.set('model', model);
+  this.render(hbs`{{activity-streams activityStreams=model}}`);
 
-  // Template block usage:
-  this.render(hbs`
-    {{#activity-streams}}
-      template block text
-    {{/activity-streams}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('.activity-streams-list ul').text().trim(), 'Foodie Blogger\n' +
+                                                                  'published a\n' +
+                                                                  '  recipe\na few seconds ago.\n\n' +
+                                                                  '        Foodie Reviewer\n' +
+                                                                  'deleted a\n' +
+                                                                  '  review\n2 hours ago.');
 });
