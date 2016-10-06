0bb873383f3055cc4ad832034498043de58bc57d
Ricky Hurtado <odatruh.ykcir@gmail.com>
2016-10-06 21:42:57 +0800

    Add test for app-footer component

--- tests/integration/components/app-footer-test.js
+++ tests/integration/components/app-footer-test.js
@@ -5,20 +5,10 @@ moduleForComponent('app-footer', 'Integration | Component | app footer', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('displays footer', function(assert) {
+  assert.expect(1);
 
   this.render(hbs`{{app-footer}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#app-footer}}
-      template block text
-    {{/app-footer}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'Foodie | Ora HQ © 2016');
 });
